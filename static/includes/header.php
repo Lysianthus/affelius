48d0b4b71e7275a451f4ce4c44ba3540b76240fc
Lysianthus <she@lysianth.us>
2017-02-26 23:03:04 +0800

    :pencil2: Fix Twitter card typo

--- static/includes/header.php
+++ static/includes/header.php
@@ -14,7 +14,7 @@
 	<meta property="og:url" content="https://affeli.us" />
 	<meta property="og:description" content="Affelius is Lysianthus’s creative repository, which showcases writings, designs, resources, tools, and labs." />
 
-	<meta name="twitter:card" content="Affelius is Lysianthus’s creative repository." />
+	<meta name="twitter:card" content="summary" />
 	<meta name="twitter:site" content="@affelius" />
 	<meta name="twitter:title" content="Affelius — Lysianthus’s Creative Repository" />
 	<meta name="twitter:description" content="Affelius is Lysianthus’s creative repository, which showcases writings, designs, resources, tools, and labs." />
