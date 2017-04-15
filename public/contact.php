2b9b416b764e9bf090d10c02edc7205d12d71a73
Lysianthus <she@lysianth.us>
2017-04-15 17:52:31 +0800

    :pencil2: Fix Twitter card

--- public/contact.php
+++ public/contact.php
@@ -22,7 +22,7 @@ include 'static/init.php';
 
 	<meta name="twitter:card" content="Affelius is Lysianthus’s creative repository." />
 	<meta name="twitter:site" content="@affelius" />
-	<meta name="twitter:title" content="Affelius — Lysianthus’s Creative Repository" />
+	<meta name="twitter:title" content="Contact | Affelius — Lysianthus’s Creative Repository" />
 	<meta name="twitter:description" content="Affelius is Lysianthus’s creative repository, which showcases writings, designs, resources, tools, and labs." />
 
 	<title>Contact | Affelius &mdash; Lysianthus’s Creative Repository</title>
